926601002b36b2ea4465c68cc34e9fd6191f8d78
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2019-08-29 16:18:43 +0300

    Implements fething admins and subscribers

--- src/app.py
+++ src/app.py
@@ -7,10 +7,17 @@ import os
 import telebot
 from flask import (
     Flask,
+    jsonify,
     request,
 )
 
-from database import create_db_tables
+from database import (
+    check_if_bp_creation_subscriber_exist,
+    create_db_tables,
+    insert_bp_creation_subscriber,
+    get_admins,
+    get_bp_creation_subscribers,
+)
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT')
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
@@ -23,9 +30,60 @@ logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
 
+def render_main_keyboard(message):
+    """
+    Render main keyboard.
+    """
+    keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+    keyboard.row('Subscribe to new block producers')
+    bot.send_message(message.from_user.id, 'Choose one of the the following keyboard buttons:', reply_markup=keyboard)
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    """
+    Start command.
+    """
+    render_main_keyboard(message)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Subscribe to new block producers', content_types=['text'])
+def handle_bp_creation_subscribing_button(message):
+    """
+    Handle user's request to check address tokens balance.
+    """
+    if not check_if_bp_creation_subscriber_exist(chat_id=message.chat.id):
+        insert_bp_creation_subscriber(chat_id=message.chat.id, nickname=message.from_user.username)
+        bot.send_message(message.chat.id, 'You have been subscribed to new block producers.')
+        return
+
+    bot.send_message(message.chat.id, 'You are already subscribed to new block producers.')
+
+
+@server.route('/subscribers/block-producer/creation')
+def get_block_producer_creation_subscribers():
+    """
+    Get list of block producer subscribers.
+    """
+    subscribers = get_bp_creation_subscribers()
+
+    if not subscribers:
+        return jsonify({'result': []})
+
+    return jsonify({'result': subscribers})
+
+
+@server.route('/administrators')
+def get_administrators():
+    """
+    Get list of administrators.
+    """
+    administrators = get_admins()
+
+    if not administrators:
+        return jsonify({'result': []})
+
+    return jsonify({'result': administrators})
 
 
 @server.route('/' + TOKEN, methods=['POST'])
--- src/database.py
+++ src/database.py
@@ -3,9 +3,13 @@ Provide implementation of database for Telegram bot.
 """
 import os
 
+import telebot
 import psycopg2
+import psycopg2.extras
 
 from utils import parse_db_url
+
+logger = telebot.logger
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
@@ -34,4 +38,63 @@ def create_db_tables():
         """
     )
 
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS bp_creation_subscribers (
+        chat_id INTEGER UNIQUE NOT NULL,
+        nickname VARCHAR (128) DEFAULT NULL);
+        """
+    )
+
     connection.commit()
+
+
+def insert_bp_creation_subscriber(chat_id, nickname):
+    """
+    Insert block producer subscriber.
+    """
+    connection = connection_to_db()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "INSERT INTO bp_creation_subscribers (chat_id, nickname) "
+        "VALUES (%s, %s);", (chat_id, nickname)
+    )
+
+    connection.commit()
+
+
+def check_if_bp_creation_subscriber_exist(chat_id):
+    """
+    Check if block producer subscriber exists by chat id.
+    """
+    connection = connection_to_db()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM bp_creation_subscribers WHERE chat_id={};".format(chat_id))
+
+    if cursor.fetchall():
+        return True
+
+    return False
+
+
+def get_bp_creation_subscribers():
+    """
+    Get block producer subscribers.
+    """
+    connection = connection_to_db()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+    cursor.execute("SELECT * FROM bp_creation_subscribers;")
+    return cursor.fetchall()
+
+
+def get_admins():
+    """
+    Get administrators.
+    """
+    connection = connection_to_db()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+    cursor.execute("SELECT * FROM admins;")
+    return cursor.fetchall()
